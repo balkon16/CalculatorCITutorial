f2a6f2bbc9802da037721321d4b6d4df0490a418
balkon16 <pawel.k.lonca@gmail.com>
2019-02-10 11:09:17 +0100

    added unit tests for calculator

--- calculator.py
+++ calculator.py
@@ -2,8 +2,10 @@
 Calculator library. Basic math operations.
 """
 
+
 def add(first, second):
     return first + second
 
+
 def subtract(first, second):
-    return first + second
+    return first - second
